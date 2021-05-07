5e56de300ddf8fa73d2bd9cc4fae411cacbdd2b1
Akhil Movva <akhilmovva6852@gmail.com>
2021-05-07 01:33:10 -0400

    updated deploy yaml

--- network-security-groups/main.tf
+++ network-security-groups/main.tf
@@ -2,3 +2,13 @@ provider "azurerm" {
   version = "=2.18.0"
   features {}
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "cloud-practice-am"
+    workspaces {
+      name = "azure-gitactions"
+    }
+  }
+}
